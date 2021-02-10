9650014ef15bb4c4bf3f60922f141acf6eb80a9a
andy.oknen <andy.oknen@gmail.com>
2021-02-10 16:56:12 +0300

    1

--- src/pocketdb/pocketmodels.h
+++ src/pocketdb/pocketmodels.h
@@ -263,6 +263,19 @@ public:
     static bool Deserialize(const string& data, PocketModel* model)
     {
         // TODO (brangr): @@@
+        //    UniValue _txs_src(UniValue::VOBJ);
+        //    _txs_src.read(pocket_data);
+        //
+        //    rtx.pTable = _txs_src["t"].get_str();
+        //    rtx.pTransaction = g_pocketdb->DB()->NewItem(rtx.pTable);
+        //    rtx.pTransaction.FromJSON(DecodeBase64(_txs_src["d"].get_str()));
+        //
+        //    if (rtx.pTable == "Mempool") {
+        //        rtx.pTable = rtx.pTransaction["table"].As<string>();
+        //        std::string _data = rtx.pTransaction["data"].As<string>();
+        //        rtx.pTransaction = g_pocketdb->DB()->NewItem(rtx.pTable);
+        //        rtx.pTransaction.FromJSON(DecodeBase64(_data));
+        //    }
 
         struct User record;
         model = &record;
--- src/primitives/pockettransaction.cpp
+++ src/primitives/pockettransaction.cpp
@@ -10,23 +10,3 @@ PocketTransaction::PocketTransaction(CMutableTransaction mtx) : CTransactionRef(
 PocketTransaction::~PocketTransaction()
 { }
 
-bool PocketTransaction::Deserialize(const std::string& data)
-{
-    // TODO (brangr): @@@ parse json to pocket model
-
-//    UniValue _txs_src(UniValue::VOBJ);
-//    _txs_src.read(pocket_data);
-//
-//    rtx.pTable = _txs_src["t"].get_str();
-//    rtx.pTransaction = g_pocketdb->DB()->NewItem(rtx.pTable);
-//    rtx.pTransaction.FromJSON(DecodeBase64(_txs_src["d"].get_str()));
-//
-//    if (rtx.pTable == "Mempool") {
-//        rtx.pTable = rtx.pTransaction["table"].As<string>();
-//        std::string _data = rtx.pTransaction["data"].As<string>();
-//        rtx.pTransaction = g_pocketdb->DB()->NewItem(rtx.pTable);
-//        rtx.pTransaction.FromJSON(DecodeBase64(_data));
-//    }
-
-    return false;
-}
